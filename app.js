b5e06e9acf9fa4fbf95d6dd3b1fca90ac3fc2c32
aleksandarskrbic <skrbic.alexa@gmail.com>
2018-05-21 14:27:21 +0200

    added rental model and routes

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require('express');
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
+const rentals = require('./routes/rentals');
 
 const app = express();
 
@@ -12,9 +13,11 @@ mongoose.connect('mongodb://localhost/movie-rent-db')
     .catch((err) => console.error('Colud not connect', err));
 
 app.use(express.json());
+
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
+app.use('/api/rentals', rentals);
 
 
 const port = process.env.PORT || 3000;
