e8981081c4094099cd322da20ea84cf798cf4cff
aleksandarskrbic <skrbic.alexa@gmail.com>
2018-05-21 14:14:37 +0200

    added movie routes to app

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 
 const genres = require('./routes/genres');
 const customers = require('./routes/customers');
+const movies = require('./routes/movies');
 
 const app = express();
 
@@ -13,6 +14,8 @@ mongoose.connect('mongodb://localhost/movie-rent-db')
 app.use(express.json());
 app.use('/api/genres', genres);
 app.use('/api/customers', customers);
+app.use('/api/movies', movies);
+
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
