6bfd60cf53adde0c52e394054060b1f5a65180b8
aleksandarskrbic <skrbic.alexa@gmail.com>
2018-05-27 19:40:43 +0200

    added users

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const genres = require('./routes/genres');
 const customers = require('./routes/customers');
 const movies = require('./routes/movies');
 const rentals = require('./routes/rentals');
+const users = require('./routes/users');
 
 const app = express();
 
@@ -21,7 +22,7 @@ app.use('/api/genres', genres);
 app.use('/api/customers', customers);
 app.use('/api/movies', movies);
 app.use('/api/rentals', rentals);
-
+app.use('/api/users', users);
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listening on port ${port}...`));
---
+++ models/user.js
@@ -0,0 +1,37 @@
+const Joi = require('joi');
+const mongoose = require('mongoose');
+
+const User = mongoose.model('User', new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+    },
+    email: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 250,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 1024
+    }
+}));
+
+function validate(user) {
+  const schema = {
+    name: Joi.string().min(5).max(50).required(),
+    email: Joi.string().min(5).max(250).required().email(),
+    password: Joi.string().min(5).max(255).required()
+  };
+
+  return Joi.validate(user, schema);
+}
+
+exports.User = User; 
+exports.validate = validate;
