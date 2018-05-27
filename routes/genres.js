3e5801e2bdbaa868f7b53ee6fcdbf331daff3c16
aleksandarskrbic <skrbic.alexa@gmail.com>
2018-05-28 01:05:44 +0200

    remove custom async middleware

--- routes/genres.js
+++ routes/genres.js
@@ -1,4 +1,3 @@
-const asyncMiddleware = require('../middleware/async');
 const auth = require('../middleware/auth');
 const admin = require('../middleware/admin');
 const { Genre, validate } = require('../models/genre');
@@ -6,12 +5,12 @@ const mongoose = require('mongoose');
 const express = require('express');
 const router = express.Router();
 
-router.get('/', asyncMiddleware(async (req, res) => {
+router.get('/', async (req, res) => {
     const genres = await Genre.find().sort('name');
     res.send(genres);
-}));
+});
 
-router.post('/', auth, asyncMiddleware(async (req, res) => {
+router.post('/', auth, async (req, res) => {
   const { error } = validate(req.body); 
   if (error) return res.status(400).send(error.details[0].message);
 
@@ -19,7 +18,7 @@ router.post('/', auth, asyncMiddleware(async (req, res) => {
   genre = await genre.save();
 
   res.send(genre);
-}));
+});
 
 router.put('/:id', async (req, res) => {
   const { error } = validate(req.body); 
